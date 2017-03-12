8998dc1660b2342f79a0fd0092c1dad538b28b54
Noah Passalacqua <noah.passa@gmail.com>
2017-03-11 19:16:37 -0500

    Added new cities.

--- app/local_data/cityData.js
+++ app/local_data/cityData.js
@@ -2,68 +2,104 @@ var cityData = [
   {
     ident: 'new-york',
     name: 'New York',
-    asset: require('../assets/new-york.png'),
-    backgroundAsset: require('../assets/new-york-background.png')
+    asset: require('../assets/cities/new-york.png'),
+    backgroundAsset: require('../assets/cities/new-york-background.png')
   },
   {
     ident: 'san-francisco',
     name: 'San Francisco',
-    asset: require('../assets/san-francisco.png'),
-    backgroundAsset: require('../assets/san-francisco-background.png')
+    asset: require('../assets/cities/san-francisco.png'),
+    backgroundAsset: require('../assets/cities/san-francisco-background.png')
   },
   {
     ident: 'london',
     name: 'London',
-    asset: require('../assets/london.png'),
-    backgroundAsset: require('../assets/london-background.png')
+    asset: require('../assets/cities/london.png'),
+    backgroundAsset: require('../assets/cities/london-background.png')
   },
   {
     ident: 'paris',
     name: 'Paris',
-    asset: require('../assets/paris.png'),
-    backgroundAsset: require('../assets/paris-background.png')
+    asset: require('../assets/cities/paris.png'),
+    backgroundAsset: require('../assets/cities/paris-background.png')
   },
   {
     ident: 'copenhagen',
     name: 'Copenhagen',
-    asset: require('../assets/copenhagen.png'),
-    backgroundAsset: require('../assets/copenhagen-background.png')
+    asset: require('../assets/cities/copenhagen.png'),
+    backgroundAsset: require('../assets/cities/copenhagen-background.png')
   },
   {
     ident: 'sydney',
     name: 'Sydney',
-    asset: require('../assets/sydney.png'),
-    backgroundAsset: require('../assets/sydney-background.png')
+    asset: require('../assets/cities/sydney.png'),
+    backgroundAsset: require('../assets/cities/sydney-background.png')
   },
   {
     ident: 'reykjavik',
     name: 'Reykjavik',
-    asset: require('../assets/reykjavik.png'),
-    backgroundAsset: require('../assets/reykjavik-background.png')
+    asset: require('../assets/cities/reykjavik.png'),
+    backgroundAsset: require('../assets/cities/reykjavik-background.png')
   },
   {
     ident: 'rome',
     name: 'Rome',
-    asset: require('../assets/rome.png'),
-    backgroundAsset: require('../assets/rome-background.png')
+    asset: require('../assets/cities/rome.png'),
+    backgroundAsset: require('../assets/cities/rome-background.png')
   },
   {
     ident: 'greece',
     name: 'Greek Islands',
-    asset: require('../assets/greece.png'),
-    backgroundAsset: require('../assets/greece-background.png')
+    asset: require('../assets/cities/greece.png'),
+    backgroundAsset: require('../assets/cities/greece-background.png')
   },
   {
     ident: 'bangkok',
     name: 'Bangkok',
-    asset: require('../assets/bangkok.png'),
-    backgroundAsset: require('../assets/bangkok-background.png')
+    asset: require('../assets/cities/bangkok.png'),
+    backgroundAsset: require('../assets/cities/bangkok-background.png')
   },
   {
     ident: 'marrakech',
     name: 'Marrakech',
-    asset: require('../assets/marrakech.png'),
-    backgroundAsset: require('../assets/marrakech-background.png')
+    asset: require('../assets/cities/marrakech.png'),
+    backgroundAsset: require('../assets/cities/marrakech-background.png')
+  },
+  {
+    ident: 'auckland',
+    name: 'Auckland, New Zealand',
+    asset: require('../assets/cities/auckland.png'),
+    backgroundAsset: require('../assets/cities/auckland-background.png')
+  },
+  {
+    ident: 'amsterdam',
+    name: 'Amsterdam, Netherlands',
+    asset: require('../assets/cities/amsterdam.png'),
+    backgroundAsset: require('../assets/cities/amsterdam-background.png')
+  },
+  {
+    ident: 'dubrovnik',
+    name: 'Dubrovnik, Croatia',
+    asset: require('../assets/cities/dubrovnik.png'),
+    backgroundAsset: require('../assets/cities/dubrovnik-background.png')
+  },
+  {
+    ident: 'cartagena',
+    name: 'Cartagena, Colombia',
+    asset: require('../assets/cities/cartagena.png'),
+    backgroundAsset: require('../assets/cities/cartagena-background.png')
+  },
+  {
+    ident: 'cape-town',
+    name: 'Cape Town, South Africa',
+    asset: require('../assets/cities/cape-town.png'),
+    backgroundAsset: require('../assets/cities/cape-town-background.png')
+  },
+  {
+    ident: 'new-orleans',
+    name: 'New Orleans, Louisiana',
+    asset: require('../assets/cities/new-orleans.png'),
+    backgroundAsset: require('../assets/cities/new-orleans-background.png')
   },
 ]
 
--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,3 +1,3 @@
 exports.API_URL = __DEV__ ?
-  'http://192.168.0.12:3000' :
+  'http://192.168.1.68:3000' :
   'https://bungee-girl-server.herokuapp.com'
